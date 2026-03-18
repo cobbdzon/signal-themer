5ae1375843bb2410716a2f44cc501c0eadeb5f44
cobbdzon <cobb@cobbdzon.cc>
2026-03-18 12:23:18 +0800

    support linux flatpak theming

--- signal_themer/__main__.py
+++ signal_themer/__main__.py
@@ -1,7 +1,8 @@
-import os 
+import os
 import platform
-import sys
 import shutil
+import sys
+
 import asarPy
 
 
@@ -13,24 +14,36 @@ def theme_injector():
 
     if os.path.isdir(temp):
         shutil.rmtree(temp)
-        
+
+    # backup asar file
+    if not os.path.isfile(app + ".bak"):
+        shutil.copy(app, app + ".bak")
+
     try:
         asarPy.extract_asar(app, temp)
     except PermissionError:
         sys.exit("PermissionError: please use the command with sudo.")
-    shutil.copy(theme_path, os.path.join(temp, "stylesheets", "theme.css")) 
+    shutil.copy(theme_path, os.path.join(temp, "stylesheets", "theme.css"))
     import_theme(os.path.join(temp, "stylesheets", "manifest.css"))
     asarPy.pack_asar(temp, app)
 
     shutil.rmtree(temp)
 
+
 # returns path of signal-desktop
 def app_path():
     match platform.system():
-        case "Linux":
+        case "Linux" if os.path.isfile("/usr/lib/signal-desktop/resources/app.asar"):
             return "/usr/lib/signal-desktop/resources/app.asar"
+        case "Linux" if os.path.isfile(
+            "/var/lib/flatpak/app/org.signal.Signal/current/active/files/Signal/resources/app.asar"
+        ):
+            return "/var/lib/flatpak/app/org.signal.Signal/current/active/files/Signal/resources/app.asar"
         case "Windows":
-            return os.path.join(os.getenv("LOCALAPPDATA"), r"Programs\signal-desktop\resources\app.asar")
+            return os.path.join(
+                os.getenv("LOCALAPPDATA"), r"Programs\signal-desktop\resources\app.asar"
+            )
+
 
 # returns temp directory depending on os in order to temporily store extracted asar
 def temp_path():
@@ -40,25 +53,28 @@ def temp_path():
         case "Windows":
             return os.path.join(os.getenv("LOCALAPPDATA"), r"temp\signal-themer")
 
+
 # add @import css in front of a file
 def import_theme(file_path):
     with open(file_path, "r+") as f:
-        if f.readline().split(" ",1)[0] != "@import":
+        if f.readline().split(" ", 1)[0] != "@import":
             f.seek(0)
         data = f.read()
         f.seek(0)
         f.truncate()
-        f.write("@import 'theme.css';\n"+data)
+        f.write("@import 'theme.css';\n" + data)
+
 
 # reads user argument to read theme path
 def selected_theme():
-    if(len(sys.argv) != 2):
+    if len(sys.argv) != 2:
         sys.exit("Wrong number of argument, try `signal-themer <path-to-theme.css>`")
-    elif(not os.path.isfile(sys.argv[1])):
+    elif not os.path.isfile(sys.argv[1]):
         sys.exit("The theme you entered doesn't exist! Try again.")
-    elif(not sys.argv[1].endswith(".css")):
+    elif not sys.argv[1].endswith(".css"):
         sys.exit("The theme must be a css file.")
     return sys.argv[1]
+
 
 if __file__ == "__main__":
     theme_injector()
